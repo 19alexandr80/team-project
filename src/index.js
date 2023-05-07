655c865a4200d6e7ce7d34e766a2858907a7e21a
19alexandr80 <usanka1980@gmail.com>
2023-05-07 06:17:06 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import "./sass/index.scss";
+
 console.log("hello world");
 console.log("gggggggggggggggg");
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpakPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[chunkhash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
   },
   devServer: {
     port: 8077,
@@ -16,5 +30,13 @@ module.exports = {
       template: "./src/index.html",
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpakPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
   ],
 };
